acd0160399cf4dda9d3ede21dd34c4fe49effee7
EC2 Default User <165451724+takamasa1993@users.noreply.github.com>
2024-04-23 04:35:45 +0000

    [4-5day-PM]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -57,7 +57,7 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
     unless current_user.id == @book.user_id
-      redirect_to book_path(@book)
+      redirect_to books_path
     end
   end
   
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
   def index
     @users = User.all
     @book = Book.new
@@ -11,25 +12,17 @@ class UsersController < ApplicationController
   end
 
   def edit
-    user = User.find(params[:id])
-    unless user.id == current_user.id
-      redirect_to user_path(@user)
-    end
     @user = User.find(params[:id])
   end
   
   def update
-    user = User.find(params[:id])
-    unless user.id == current_user.id
-      redirect_to user_path(@user)
-    end
     @user = User.find(params[:id])
     if @user.update(user_params)
       @user.reload
       flash[:success] = "You have updated user successfully！"
       redirect_to user_path(@user)
     else
-      flash[:error] = "更新失敗。error"
+      flash.now[:error] = "profile update error"
       render :edit
     end
   end
@@ -38,6 +31,13 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :profile_image, :introduction)
+  end
+  
+  def is_matching_login_user
+    @user = User.find(params[:id])
+    unless @user.id == current_user.id
+      redirect_to user_path(current_user)
+    end
   end
   
 end
